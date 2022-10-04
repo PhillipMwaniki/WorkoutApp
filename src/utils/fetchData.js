002cd6f6ad10156e81e4207b3ccdba769b719e34
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-10-04 10:39:14 +0300

    added youtube search options and key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
     }
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.YOUTUBE_SEARCH,
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     return await response.json();
