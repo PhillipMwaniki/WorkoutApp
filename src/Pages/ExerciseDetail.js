5b62c7fb55210fccc325d1388d6dcbb0edb78d75
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-10-04 10:39:04 +0300

    Added fetch request to get videos

--- src/Pages/ExerciseDetail.js
+++ src/Pages/ExerciseDetail.js
@@ -5,11 +5,12 @@ import { Box } from "@mui/material";
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 
 
 const ExerciseDetail = () => {
     const [exerciseDetail, setExerciseDetail] = useState({})
+    const [exercisesVideos, setExercisesVideos] = useState({})
     const { id } = useParams();
 
     useEffect(() => {
@@ -19,6 +20,8 @@ const ExerciseDetail = () => {
 
             const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
             setExerciseDetail(exerciseDetailData);
+            const exercisesVideoData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`, youtubeOptions);
+            setExercisesVideos(exercisesVideoData);
       }
 
       fetchExercisesData();
@@ -28,7 +31,7 @@ const ExerciseDetail = () => {
     return (
         <div>
             <Detail exerciseDetail={exerciseDetail} />
-            <ExerciseVideos />
+            <ExerciseVideos exercisesVideos={exercisesVideos} name={exerciseDetail.name} />
             <SimilarExercises />
         </div>
     );
